47da0cb73427b7ab4fd46813fdd6a8477ecf9a33
usuario <usuario@10.22.146.211>
2014-02-20 12:29:07 +0000

    First Commit

---
+++ src/NumberCounter/MainCounter.java
@@ -0,0 +1,28 @@
+package NumberCounter;
+import java.util.Scanner;
+
+public class MainCounter {
+//2,3,4,5,5,9,2,9,8,5,7,3,7,4
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        boolean condicion = true;
+        int selector;
+        System.out.print("Bienvenid@ al programa para contar apariciones del");
+        System.out.println(" mismo número en un vector.");
+        while (condicion){
+            System.out.println("Puedes elegir que programa quieres ejecutar:");
+            System.out.println("1) NumberCounter1");
+            System.out.println("0) Salir de la ejecución");
+            selector = teclado.nextInt();
+            System.out.println("");
+            switch (selector) {
+                case 1: {
+                    int[] v;
+                    v = OpVector.InicializaVector();
+                    NumberCounter1.NumberCounter1(v);
+                }
+                case 0: condicion = false;
+            }
+        }
+    }
+}
